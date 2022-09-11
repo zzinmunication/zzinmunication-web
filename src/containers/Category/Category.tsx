a67b4a6c7b2a55439efb472bcb975766167da834
Teddy <teddy.ryan@kakaostyle.com>
2022-09-11 20:56:16 +0900

    feat: public의 json 파일 읽어서 렌더링 적용 => 이후 recoil에 반영

--- src/containers/Category/Category.tsx
+++ src/containers/Category/Category.tsx
@@ -1,7 +1,13 @@
 import DefaultLayout from "components/DefaultLayout";
-import React, { FunctionComponent, useState } from "react";
+import React, {
+  FunctionComponent,
+  useState,
+  useEffect,
+  useMemo
+} from "react";
 import { Input, List, Typography } from 'antd';
 import { useDebounce } from 'use-debounce';
+import { ListHover } from "./CategoryListStyles";
 
 interface ICategoryProps {
 }
@@ -14,26 +20,38 @@ const data = [
   'Los Angeles battles huge wildfires.',
 ];
 
-const onSearch = (value: string) => console.log(value);
-
 const Category: FunctionComponent<ICategoryProps> = (props) => {
   const [search, setSearch] = useState<string>("")
   const [debounceSearch] = useDebounce(search, 500)
+  const [data, setData] = useState({})
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  useEffect(async () => {
+    const response = await fetch('/zzinmunication_dataset.json')
+    const data = await response.json()
+    console.log('data', data)
+    setData(data)
+  }, [])
+
+  const dataKeys = useMemo(() => Object.keys(data), [data])
 
   return (
     <DefaultLayout>
       <Input placeholder="input search text" size="large" onChange={(e: any) => setSearch(e?.target?.value)} />
-      <List
-        style={{ background: 'white', marginTop: 10 }}
-        header={<div>검색내용: {debounceSearch}</div>}
-        bordered
-        dataSource={data.filter(v => v.toLowerCase().includes(debounceSearch.toLowerCase()))}
-        renderItem={item => (
-          <List.Item>
-            <Typography.Text mark>[ITEM]</Typography.Text> {item}
-          </List.Item>
-        )}
-      />
+      <ListHover>
+        <List
+          style={{ background: 'white', marginTop: 10 }}
+          header={<div>검색내용: {debounceSearch}</div>}
+          bordered
+          dataSource={dataKeys.filter(v => v.toLowerCase().includes(debounceSearch.toLowerCase()))}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </ListHover>
     </DefaultLayout>
   );
 };
---
+++ src/containers/Category/CategoryListStyles.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import propsToStyle from "utils/propsToStyle";
+
+interface Props {
+  style?: any
+}
+
+export const ListHover: any = styled.div`
+  ul.ant-list-items {
+    & > li.ant-list-item:hover {
+      background-color: rgba(0, 0, 0, 0.1);
+
+      &:last-child {
+        border-radius: 0px 0px 10px 10px;
+      }
+    }
+  } 
+  
+
+  ${(props: Props) => propsToStyle(props.style || {})}
+`
